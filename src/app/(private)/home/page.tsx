a1c9c1eae2dfde999cac75f66c56aa78915f78bf
muhammad munir ishak <moo_nir@yahoo.com>
2023-10-22 02:58:24 +0800

    add key

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4">
       {users?.map((user: UserType, idx: number) => (
-        <Card className="m-3 md:">
+        <Card key={"users"+user.id} className="m-3 md:">
           <div key={user.id} className="flex flex-col items-center pb-10">
             <Avatar img={user.avatar} size="xl" />
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
