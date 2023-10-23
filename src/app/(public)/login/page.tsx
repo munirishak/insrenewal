09d981e81fc138135804e3b8d2f8acccfaabbf1d
muhammad munir ishak <moo_nir@yahoo.com>
2023-10-23 16:49:25 +0800

    restructure the code

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,36 +1,7 @@
-"use client";
-
-import { setAuth, setSession } from "@/app/store/slices/authSlice";
-import { Button } from "flowbite-react";
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import Login from "@/controller/auth";
 
 export default function LoginPage() {
-  const [isLoading, setIsLoading] = useState(false)
-  const { data: session, status } = useSession();
-  const router = useRouter();
-  const dispatch = useDispatch();
-
-  const handleLogin = () => {
-    signIn("google")
-  }
-
-  useEffect(() => {
-    setIsLoading(status === "loading");
-
-    if (status === "authenticated") {
-      dispatch(setSession(session));
-      dispatch(setAuth(true));
-      router.replace("home");
-    }
-
-  },[status]);
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={handleLogin} isProcessing={isLoading} disabled={isLoading}>Login With Google</Button>
-    </main>
+    <Login/>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextAuthProvider from "@/components/NextAuthProvider";
-import ReduxProvider from "./store/reduxProvider";
+import ReduxProvider from "@/store/reduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      
         <NextAuthProvider><ReduxProvider>{children}</ReduxProvider></NextAuthProvider>
-        
       </body>
     </html>
   );
